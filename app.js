6bb4429f913313ef638e672d9bd5137973ed30ca
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-02-05 17:32:50 +0700

    setup env

--- app.js
+++ app.js
@@ -5,26 +5,12 @@ var app = express();
 
 const routes = require("./routes");
 
-const sequelize = require("#config/database");
 
-const { port } = require("#config/vars");
 const { verifyApiKey } = require("#middlewares/auth");
-const PORT = port || 5000;
 
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 
-// view engine setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- config/vars.js
+++ config/vars.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  port: process.env.PORT,
+  portConn: process.env.PORT,
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
   dbPass: process.env.DB_PASS,
