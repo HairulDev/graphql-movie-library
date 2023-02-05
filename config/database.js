f1c761d60372651660f9984234537ebebca0b560
Hairul <98758226+HairulDev@users.noreply.github.com>
2023-02-05 19:16:38 +0700

    resolve pg install manual

--- config/database.js
+++ config/database.js
@@ -1,8 +1,11 @@
 "use strict";
 const { Sequelize } = require("sequelize");
+const pg = require("pg");
 
 const { dbUser, dbPass, dbHost, dbPort, dbName } = require("#config/vars");
 const DB_URL = `postgres://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
 
-const sequelize = new Sequelize(DB_URL);
+const sequelize = new Sequelize(DB_URL, {
+    dialectModule: pg
+});
 module.exports = sequelize;
